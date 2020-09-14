70ff8fec4eb58bd11bbe1768f66f9ba8fe97ac18
Federico Aloi <federico.aloi@gmail.com>
2020-09-13 23:17:04 -0300

    Eliminada referencia a la Calculadora en el REPL

--- src/test/kotlin/ar/edu/unahur/obj2/ejemplo/repl.ws.kts
+++ src/test/kotlin/ar/edu/unahur/obj2/ejemplo/repl.ws.kts
@@ -1,10 +1,7 @@
-import ar.edu.unahur.obj2.ejemplo.Calculadora
-
 // Pueden usar este archivo para hacer pruebas rápidas,
 // de la misma forma en que usaban el REPL de Wollok.
 
 // OJO: lo que esté aquí no será tenido en cuenta
 // en la corrección ni reemplaza a los tests.
 
-Calculadora(10, 2).suma()
-Calculadora(10, 2).division()
+listOf(1, 8, 10).average()
