4e72de7ee6f80874f3dfbbcdf06bfd7b63fd87c0
Federico Aloi <federico.aloi@gmail.com>
2021-05-12 12:24:05 -0300

    Configuración para GH actions

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/KotestConfig.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/KotestConfig.kt
@@ -2,8 +2,12 @@ package ar.edu.unahur.obj2.impostoresPaises
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
+import io.kotest.extensions.junitxml.JunitXmlReporter
 
 class KotestConfig: AbstractProjectConfig() {
   // Modificamos esta configuración para que se creen nuevos objetos para cada `it`.
   override val isolationMode = IsolationMode.InstancePerLeaf
+
+  // Reporta el resultado de los tests como XML, lo cual usamos para que se muestren en el pull request cuando fallan.
+  override fun listeners() = listOf(JunitXmlReporter())
 }
